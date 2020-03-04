b2c70f69c11cbe897af642c8672e5f6e2ab90e62
Lartum <lartumraksap11@live.com>
2020-03-04 15:30:59 +0530

    Creating payment

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ import Newpassword from './components/auth/Newpassword';
 import Forgotpassword from './components/auth/Forgotpassword';
 import Application from './components/application/Application';
 import Pdf from './components/application/Pdf'
-import Imageupload from './Imageupload';
+// import Imageupload from './Imageupload';
 import Userdashboard from './components/userdashboard/Userdashboard';
 import Logincard from './components/auth/Logincard'
 
