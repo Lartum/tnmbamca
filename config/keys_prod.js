b972ca602e45a1516caa71d91e80c0de377ad3cb
Lartum <lartumraksap11@live.com>
2020-05-21 14:19:24 +0530

    revealing the keys

---
+++ config/keys_prod.js
@@ -0,0 +1,25 @@
+module.exports = {
+    //Mongo DB key
+    mongoURI: 'mongodb+srv://lartum:Lartum@cluster0-qij9g.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true',
+  
+    //JWT key
+    secretOrKey: 'secret',
+  
+    //AWS key
+    AWS_ACCESS_KEY_ID:'AKIA2WV5YVMREI4JYBJS',
+    AWS_SECRET_ACCESS_KEY:'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',
+    AWS_REGION:'ap-southeast-1',
+    
+    
+    //AWS S3 Image Bucket Key 
+     ACCESS_KEY_ID: 'AKIAJE4C7O74WGLDRU5A',
+     SECRET_ACCESS_KEY: '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',
+     REGION: 'ap-southeast-1', 
+    
+     //Sendgrid Key
+    SENDGRID_USER: 'Lartum',
+    SENDGRID_PASS: 'nokialumia520',
+  
+    RAZORPAY_KEY_ID: 'rzp_test_4hx0rrE95gnSNE',
+    RAZORPAY_SECRET: 'c1HvMQQA16IL6ba6971JlQuQ'
+};
---
+++ config/sendgrid.js
@@ -0,0 +1,3 @@
+module.exports = {
+sendgridkey : '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'
+}
